511ab6c6b648f17114064506697ea0737647e8cf
MatheusNishiyama <matheus_nishiyama@hotmail.com>
2017-04-02 20:37:12 +0100

    Criação de StoryBoards, estruturação de classes, Leitor de PDF na Primeira Tab.

--- CCGApp/CCGApp/NewsTableViewController.swift
+++ CCGApp/CCGApp/NewsTableViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import QuickLook
 
-class NewsTableViewController: UITableViewController {
+class NewsTableViewController: UITableViewController, QLPreviewControllerDataSource {
     
     var allNews = [News]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Notícias"
@@ -20,33 +21,33 @@ class NewsTableViewController: UITableViewController {
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return allNews.count
         
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Configure the cell...
-
+        
         let cellIdentifier = "Cell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! NewsTableViewCell
         
@@ -57,57 +58,94 @@ class NewsTableViewController: UITableViewController {
         let year = calendar.component(.year, from: date!)-2000
         let month = calendar.component(.month, from: date!)
         
-    
-                
+        
+        
         cell.dateCell.text = "\(month)/\(year)"
         
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    //MARK: QuickLook DataSources
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int{
+        
+        return allNews.count
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    
+    //MARK: Funcao Select
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath){
+        
+        let previewPdf = QLPreviewController()
+        previewPdf.dataSource = self
+        previewPdf.currentPreviewItemIndex = indexPath.row
+        show(previewPdf, sender: nil)
+        
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem{
+        let filePath = allNews[index].url
+        
+        return filePath as! QLPreviewItem
     }
-    */
-
+    
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    /*
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
+     let indexPath = self.tableView.indexPathForSelectedRow
+     
+     if segue.identifier == "SegueToPdf" {
+     let news = allNews[(indexPath?.row)!]
+     
+     let controller = segue.destination as! UrlWebViewController
+     controller.news = news
+     
+     }
+     
+     
+     }
+     */
 }
