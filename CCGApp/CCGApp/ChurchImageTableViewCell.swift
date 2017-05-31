8f872a01727df4d66dde39b644c99446e21b3914
MatheusNishiyama <matheus_nishiyama@hotmail.com>
2017-05-31 19:07:48 +0100

    Correção de bug na view 'Reuniões' da tela 'Saiba mais' - > Problema na TableView

---
+++ CCGApp/CCGApp/ChurchImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChurchImageTableViewCell.swift
+//  CCGApp
+//
+//  Created by Matheus Nishi on 31/05/17.
+//  Copyright © 2017 Matheus Nishi. All rights reserved.
+//
+
+import UIKit
+
+class ChurchImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CCGApp/CCGApp/SundayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SundayTableViewCell.swift
+//  CCGApp
+//
+//  Created by Matheus Nishi on 31/05/17.
+//  Copyright © 2017 Matheus Nishi. All rights reserved.
+//
+
+import UIKit
+
+class SundayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
